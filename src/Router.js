f2180f927fbbe02e85389d7ded529da6bb4c2eed
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-07-29 16:55:57 +0530

    90

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./User/Signup";
 import Signin from "./User/Signin";
 import Home from "./core/Home";
+import Menu from "./core/Menu";
+import App from "./App";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Menu />
       <Switch>
         <Route path="/" exact component={Home} />{" "}
         {/* setting the path to "/" so that the main landing page is home. */}
         <Route path="/signup" exact component={Signup} />
+        <Route path="/app" exact component={App} />
         <Route path="/signin" exact component={Signin} />
       </Switch>
     </BrowserRouter>
---
+++ src/core/Menu.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+//react-router-dom is basically a file from react to take care of the routing within our app.
+
+//creating a sort of middelware for changing the color of the button tab.
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
+  } else {
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = (
+  { history } //getting the history from props. By destructuring. instead of props.history
+) => (
+  <div>
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, "/")} to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signin")}
+          to="/signin"
+        >
+          SignIn
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signup")}
+          to="/signup"
+        >
+          SignUp
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default withRouter(Menu);
