3480c217ef47289861f1913a34b2c36844b8f013
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-12-04 00:35:35 +0530

    creating_categories

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,12 +1,37 @@
 import React,{useState,useEffect} from "react";
 import Layout from "./layout";
 import Card from './card'
+import {getCategories} from './apiCore'
+import CheckBox from './checkBox'
 
 const Shop=()=>{
+    const [categories, setCategories]=useState([])
+    const [error, setError]=useState(false)
+
+     //load categories and set formData
+     const init = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                setCategories(data)
+            }
+        });
+    };
+
+    useEffect(()=>{
+        init()
+    },[])
+
     return(
         <Layout title="Shop page" description="Search and find your books!" className='container-fluid'>
             <div className='row'>
-                <div className='col-4'>left sidebar</div>
+                <div className='col-4'>
+                    <h4>Filter by categories!</h4>
+                    <ul>
+                    <CheckBox categories={categories}/>
+                    </ul>
+                </div>
                 <div className='col-8'>right</div>
             </div>
         </Layout>
