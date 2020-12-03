99b3d0ece0afe3fdd932c91e803f02ee97b88118
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-12-03 23:47:36 +0530

    image-not-displaying

---
+++ src/core/showImage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {API} from '../config'
+
+const ShowImage=()=>(
+    <div className='product-img'>
+        <image src={'../images/node1.png'}  className='mb-3' style={{maxHeight:'100%',maxWidth:'100%'}}/>
+    </div>
+)
+
+export default ShowImage
